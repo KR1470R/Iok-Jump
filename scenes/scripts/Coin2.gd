558039db43ec9efeeb30995fe4da141145c5d7a3
KR1470R <vartus2@gmail.com>
2019-12-22 01:24:11 +0200

    Fix4

--- scenes/scripts/Coin2.gd
+++ scenes/scripts/Coin2.gd
@@ -3,14 +3,67 @@ extends KinematicBody2D
 var screenSize = Vector2(0,0)
 var rng = RandomNumberGenerator.new()
 var speed = 1
+var boost_cheker = "user://boost_cheker.txt"
+var path_rocket = "user://used_ship.txt"
 
+var BOTTOM = Vector2(0,1)
 func _physics_process(delta):
 	rng.randomize()
+	
+	var fr = File.new()
+	fr.open(boost_cheker, File.READ)	
+	var string = fr.get_line()
+	
+	var fr2 = File.new()
+	fr2.open(path_rocket,File.READ)
+	var string_rocket = fr2.get_line()
+	
 	screenSize.x = get_viewport().get_visible_rect().size.x # Get Width
 	screenSize.y = get_viewport().get_visible_rect().size.y # Get Width
 	var rpos = rng.randf_range(0,screenSize.x-10)
-	
-	self.position.y += 7*speed
+
+	if int(string_rocket) == 1:
+		if self.position.y != get_viewport().size.y - 50:
+			move_and_collide(BOTTOM*speed*4.7)
+		if string == "True":
+			move_and_collide(BOTTOM*speed*5)
+		elif string == "False":
+			move_and_collide(BOTTOM*speed*5)
+	if int(string_rocket) == 2:
+		if self.position.y != get_viewport().size.y - 50:
+			move_and_collide(BOTTOM*speed*4.8)
+		if string == "True":
+			move_and_collide(BOTTOM*speed*5)
+		elif string == "False":
+			move_and_collide(BOTTOM*speed*4.8)
+	if int(string_rocket) == 3:
+		if self.position.y != get_viewport().size.y - 50:
+			move_and_collide(BOTTOM*speed*4.85)
+		if string == "True":
+			move_and_collide(BOTTOM*speed*5)
+		elif string == "False":
+			move_and_collide(BOTTOM*speed*4.85)
+	if int(string_rocket) == 4:
+		if self.position.y != get_viewport().size.y - 50:
+			move_and_collide(BOTTOM*speed*4.9)
+		if string == "True":
+			move_and_collide(BOTTOM*speed*5)
+		elif string == "False":
+			move_and_collide(BOTTOM*speed*4.9)
+	if int(string_rocket) == 5:
+		if self.position.y != get_viewport().size.y - 50:
+			move_and_collide(BOTTOM*speed*5)
+		if string == "True":
+			move_and_collide(BOTTOM*speed*5)
+		elif string == "False":
+			move_and_collide(BOTTOM*speed*5)
+	if int(string_rocket) == 6:
+		if self.position.y != get_viewport().size.y - 50:
+			move_and_collide(BOTTOM*speed*5)
+		if string == "True":
+			move_and_collide(BOTTOM*speed*6)
+		elif string == "False":
+			move_and_collide(BOTTOM*speed*5)
 	if self.position.y >= screenSize.y+100:
 		self.position = Vector2(rpos,-50)
 		self.show()
